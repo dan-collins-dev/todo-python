092ee0d6e1e7950fa5de3f60dc395fa9dc2861bd
Dan Collins <dan.collins.dev@gmail.com>
2024-10-16 11:44:54 -0400

    added search by terms functionality

--- app.py
+++ app.py
@@ -3,41 +3,40 @@ class App:
     def __init__(self):
         self.running = False
         self.todos = []
-    
+        
     def initialize(self):
-        todo1 = Todo("Todo 1", "Todo 1 Description")
-        todo2 = Todo("Todo 2", "Todo 2 Description")
-        todo3 = Todo("Todo 3", "Todo 3 Description")
-        todo4 = Todo("Todo 4", "Todo 4 Description")
+        # Testing todos
+        # todo1 = Todo("Todo 1", "Todo 1 Description", self)
+        # self.todos.append(todo1)
+        # todo2 = Todo("Todo 2", "Todo 2 Description", self)
+        # self.todos.append(todo2)
+        # todo3 = Todo("Todo 3", "Todo 3 Description", self)
+        # self.todos.append(todo3)
+
+        # todo4 = Todo("Clean the dishes", "Dishes are piling up", self)
+        # self.todos.append(todo4)
+        # todo5 = Todo("Do Dishes", "Testing the lower case", self)
+        # self.todos.append(todo5)
+        # todo6 = Todo("Help neighbors", "They need help", self)
+        # self.todos.append(todo6)
         
-        self.todos.append(todo1)
-        self.todos.append(todo2)
-        self.todos.append(todo3)
-        self.todos.append(todo4)
+        # self.print_todos()
         
-        self.print_todos()
-        
-        print("===========================================================")
-        print("Todo App in Python (console version) written by Dan Collins")
-        print("===========================================================\n\n")
-        
-        print("//Todo App")
-        print("What would you like to do?")
-        print("1. Manage Todos")
-        print("2. Quit Application")
+        self.display_app_menu()
         choice = input("Choose your option (1 or 2): ")
         
         match choice:
             case "1":
+                self.running = True
                 self.run()
             
             case "2":
                 print("Quitting application")
     
-    def add_todo(self):
+    def update_todo(self, id):
         pass
     
-    def remove_todo(self):
+    def remove_todo(self, id):
         pass
     
     def clear_todos(self):
@@ -46,30 +45,68 @@ class App:
     def save_todos(self):
         pass
     
-    def read_todos(self):
-        pass
+    def search_todos(self):
+        search_string = input("\nPlease enter your search terms: ")
+        search_terms = search_string.split()
+        results = []
+        
+        for term in search_terms:
+            term = term.lower()
+
+        for todo in self.todos:
+            if term in todo.name.lower() or term in todo.description.lower() or term in str(todo.id):
+                results.append(todo)
+        
+        print("\n====== Search Results ======")
+        for result in results:
+            print(result)
+        print("============================")
+            
     
     def print_todos(self):
         for todo in self.todos:
             print(todo)
+            
     
     def run(self):
-        self.running = True
-        if (self.running):
+        while (self.running):
             self.display_main_menu()
-            choice = input("Choose your option (1, 2, 3, 4, 5): ")
+            choice = input("Choose your option (1, 2, 3, 4, 5, or 6): ")
             
             match choice:
-                case "5":
+                case "1":
+                    self.display_create_menu()
+                    
+                case "2":
+                    self.find_todo()
+                
+                case "4":
+                    self.print_todos()
+                
+                case "6":
                     self.running = False
     
+    def display_app_menu(self):
+        print("===========================================================")
+        print("Todo App in Python (console version) written by Dan Collins")
+        print("===========================================================\n\n")
+        
+        print("//Todo App")
+        print("What would you like to do?")
+        print("1. Manage Todos")
+        print("2. Quit Application")
+        
     def display_main_menu(self):
         print("\nWhat would you like to do?")
         print("1. Create a Todo")
-        print("2. Update a Todo")
-        print("3. Remove a Todo")
-        print("4. List all Todos")
-        print("5. Quit Application")
+        print("2. Update a Todo By ID")
+        print("3. Search for Todo")
+        print("4. Remove a Todo By ID")
+        print("5. List all Todos")
+        print("6. Quit Application")
     
     def display_create_menu(self):
-        pass
+        print("\nPlease Enter a name and description for your todo.")
+        title = input("Name: ")
+        desc = input("Description: ")
+        self.todos.append(Todo(title, desc, self))
--- todo.py
+++ todo.py
@@ -1,14 +1,27 @@
 class Todo:
-    def __init__(self, name, description):
+    def __init__(self, name, description, parent):
+        
         self.name = name
         self.description = description
+
+        # Inner function to create a simple id for each todo
+        def assign_id(self, _parent):
+            if len(_parent.todos) == 0:
+                print("test")
+                return 1
+            else:
+                return len(_parent.todos) + 1
+                
+        self.id = assign_id(self, parent)
+
               
     def __str__(self):
-        return f"Todo Name: {self.name}\nTodo Description: {self.description}\n"
+        return f"\nTodo ID: {self.id}\nTodo Name: {self.name}\nTodo Description: {self.description}\n"
     
     def __repr__(self):
         return (
             f"Todo("
+            f"{repr(self.id)}, "
             f"{repr(self.name)}, "
             f"{repr(self.description)})"
         )
